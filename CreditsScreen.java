ca7fcfb32a169e962fe1f97ac9dbae80578c2f52
TristinH <tristin95@yahoo.com>
2016-08-08 22:59:54 -0700

    Add files via upload

---
+++ CreditsScreen.java
@@ -0,0 +1,61 @@
+/*************************************************************************
+ *      File Name: CreditsScreen.java
+ *      Authors: Tristin Hrafnsson, Darryl Occ,
+ *               Ashkan Ghafari
+ *      Class: CS 245 - Programming Graphical User Interfaces
+ * 
+ *      Assignment: Quarter Project - Checkpoint 1
+ *      Date Last Modified: 8/8/2016
+ * 
+ *      Purpose: Display the credits to the user. Each groups member's
+ *      name and bronco ID is displayed.
+ *************************************************************************/
+package Hangman;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *  Class to display the group's names and IDs
+ */
+public class CreditsScreen extends JPanel{
+    
+    //no-arg constructor
+    public CreditsScreen() {
+        //set the background to green
+        setBackground(Color.GREEN);
+    }
+    
+    //method: getPreferredSize
+    //purpose: give the prefereed size of the panel to the
+    //layout manager.
+    public Dimension getPreferredSize() {
+        return new Dimension(600, 400);
+    }
+    
+    //method: paintComponent
+    //purpose: override the super class method to customize
+    //the panel to our specifications
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        //customize the font to be displayed
+        Font font = new Font("Broadway", Font.PLAIN, 32);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setFont(font);
+        //turn on rendering hints
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+                            RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(Color.WHITE);
+        //draw the title of the screen
+        g2.drawString("CREDITS", 220, 50);
+        
+        //change the font for the names
+        font = new Font("Times New Roman", Font.PLAIN, 20);
+        g2.setFont(font);
+        //display the information on the screen
+        g2.drawString("TRISTIN HRAFNSSON, 009821123", 140, 150);
+        g2.drawString("DARYL OCC, 010475647", 140, 190);
+        g2.drawString("ASHKAN GHAFARI, 009146475", 140, 230);
+    }
+}
---
+++ TitleScreen.java
@@ -0,0 +1,61 @@
+/*************************************************************************
+ *      File Name: TitleScreen.java
+ *      Authors: Tristin Hrafnsson, Darryl Occ,
+ *               Ashkan Ghafari
+ *      Class: CS 245 - Programming Graphical User Interfaces
+ * 
+ *      Assignment: Quarter Project - Checkpoint 1
+ *      Date Last Modified: 8/8/2016
+ * 
+ *      Purpose: Create a title screen to show basic team and project
+ *      information. This screen is displayed for 3 seconds and leads
+ *      into the option screen.
+ *************************************************************************/
+package Hangman;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * This class extends JPanel and uses inherited properties to
+ * display team information.
+ */
+public class TitleScreen extends JPanel {
+    
+    //no-arg constructor
+    public TitleScreen() {
+        //set the background to be black
+        setBackground(Color.BLACK);
+    }
+    
+    //method: getPreferredSize
+    //purpose: give the prefereed size of the panel to the
+    //layout manager.
+    public Dimension getPreferredSize() {
+        return new Dimension(600, 400);
+    }
+    
+    //method: paintComponent
+    //purpose: override the super class method to customize
+    //the panel to our specifications
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        //customize the font 
+        Font font = new Font("Times New Roman", Font.PLAIN, 32);
+        Graphics2D g2 = (Graphics2D) g; //cast to 2D graphics object
+        //set the font and turn on rendering hints
+        g2.setFont(font);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+                            RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(Color.WHITE);
+        //draw the project title to the panel
+        g2.drawString("CS 245 Quarter Project", 140, 50);
+        
+        //customize the font again
+        font = new Font("Times New Roman", Font.PLAIN, 20);
+        g2.setFont(font);
+        //display team information
+        g2.drawString("By: Team Dashkan", 220, 370);
+    }
+}
